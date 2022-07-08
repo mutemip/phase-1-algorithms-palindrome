8532173a1c936968760c2a634b91e2d4790f8b8d
mutemip <paulmutemi2030@gmail.com>
2022-07-08 15:53:28 +0300

    used a new way to solve the palindrome task

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 function isPalindrome(test) {
-    for (let i = 0; i < test.length / 2; i++) {
-      const j = test.length - 1 - i;
-      if (test[i] !== test[j]) return false;
-    }
-    return true;
+
+  let newTest = test;
+  let reverseTest = newTest.split("").reverse().join("");
+  return (newTest === reverseTest) ? true:false;
+    // for (let i = 0; i < test.length / 2; i++) {
+    //   const j = test.length - 1 - i;
+    //   if (test[i] !== test[j]) return false;
+    // }
+    // return true;
 }
 
 /* 
   Add your pseudocode here
+
 */
 
 /*
   Add written explanation of your solution here
+    - declare a new variable (newTest) and give it the value of the argument;
+  - declare another variable (reverseTest) to revrse the (newTest) value using string methods
+  - use condition to know if the requirements are met 
 */
 
 // You can run `node index.js` to view these console logs
